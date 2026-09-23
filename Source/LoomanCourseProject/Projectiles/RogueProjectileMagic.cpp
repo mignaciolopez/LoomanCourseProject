64f406c3f24e236901d8a4f8c182e903eb71e6c3
Martin Ignacio Lopez <lmartinignacio@gmail.com>
2026-09-23 14:14:04 -0300

    Collision & Physics: Actor & Components (Projectile Attack)

---
+++ Source/LoomanCourseProject/Projectiles/RogueProjectileMagic.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RogueProjectileMagic.h"
+
+#include "NiagaraComponent.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+// Sets default values
+ARogueProjectileMagic::ARogueProjectileMagic()
+{
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	RootComponent = SphereComponent;
+	SphereComponent->SetSphereRadius(16.0f);
+	SphereComponent->SetCollisionProfileName(TEXT("Projectile"));
+
+	LoopedNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("LoopedNiagaraComponent"));
+	LoopedNiagaraComponent->SetupAttachment(SphereComponent);
+
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+	ProjectileMovementComponent->InitialSpeed = 2000.f;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
+
+}
+
