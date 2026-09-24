3a799b07f8ef361dd2a573cafaed7973820afa79
Martin Ignacio Lopez <lmartinignacio@gmail.com>
2026-09-24 12:27:32 -0300

    Interaction Component & Collision Traces - Interactable Item Chest

--- Source/LoomanCourseProject/Word/RogueItemChest.cpp
+++ Source/LoomanCourseProject/Word/RogueItemChest.cpp
@@ -4,16 +4,35 @@
 #include "RogueItemChest.h"
 
 
-// Sets default values
 ARogueItemChest::ARogueItemChest()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	BaseMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMeshComponent"));
+	RootComponent = BaseMeshComponent;
+
+	LidMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LidMeshComponent"));
+	LidMeshComponent->SetupAttachment(BaseMeshComponent);
 }
 
-// Called every frame
+void ARogueItemChest::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetActorTickEnabled(true);
+}
+
 void ARogueItemChest::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	CurrentAnimationPitch = FMath::FInterpConstantTo(CurrentAnimationPitch, AnimationTargetPitch, DeltaTime, AnimationSpeed);
+	LidMeshComponent->SetRelativeRotation(FRotator(CurrentAnimationPitch, 0.0f, 0.0f));
+
+	if (FMath::IsNearlyEqual(CurrentAnimationPitch, AnimationTargetPitch))
+	{
+		SetActorTickEnabled(false);
+	}
 }
 
--- Source/LoomanCourseProject/Word/RogueItemChest.h
+++ Source/LoomanCourseProject/Word/RogueItemChest.h
@@ -11,11 +11,28 @@ class LOOMANCOURSEPROJECT_API ARogueItemChest : public AActor
 {
 	GENERATED_BODY()
 
-public:
-	// Sets default values for this actor's properties
-	ARogueItemChest();
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Comopnents")
+	TObjectPtr<UStaticMeshComponent> BaseMeshComponent;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Comopnents")
+	TObjectPtr<UStaticMeshComponent> LidMeshComponent;
+
+	UPROPERTY(EditAnywhere, Category = "Animation")
+	float AnimationSpeed = 50.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Animation")
+	float AnimationTargetPitch = 120.0f;
+
+	float CurrentAnimationPitch = 0.0f;
+
+	virtual void BeginPlay() override;
 
 public:
-	// Called every frame
+	ARogueItemChest();
+
 	virtual void Tick(float DeltaTime) override;
+
+
 };
